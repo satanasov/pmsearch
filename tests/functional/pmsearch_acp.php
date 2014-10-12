68b5e8479132c506c0cbcc72a12d9285449f6c5b
Stanislav Atanasov <lucifer@anavaro.com>
2014-10-12 13:01:37 +0300

    Add tests for event index

--- tests/functional/pmsearch_acp.php
+++ tests/functional/pmsearch_acp.php
@@ -31,6 +31,15 @@ class pmsearch_acp extends pmsearch_base
 		
 		$this->logout();
 	}
-	
+	public function test_event_auto_index()
+	{
+		$this->login();
+		$message_id = $this->create_private_message('Test private message #1', 'This is a test private message sent by the testing framework. We need to check event indexing.', array($this->get_user_id('testuser1')));
+		
+		$this->add_lang_ext('anavaro/pmsearch', 'info_acp_pmsearch');
+		$crawler = self::request('GET', 'adm/index.php?i=-anavaro-pmsearch-acp-acp_pmsearch_module&mode=main&sid=' . $this->sid);
+		
+		$this->assertContains(3, $crawler->text());
+	}
 }
 ?>
