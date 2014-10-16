9a872c36aa85d60a9e4167a5e9b0bad5aea92ad3
Stanislav Atanasov <lucifer@anavaro.com>
2014-10-16 08:18:10 +0300

    Initial delete pm test

--- tests/functional/pmsearch_acp_test.php
+++ tests/functional/pmsearch_acp_test.php
@@ -112,5 +112,19 @@ class pmsearch_acp_test extends pmsearch_base
 		//$this->assertContains('alalaalalalalala', $crawler->text());
 		$this->logout();
 	}
+	public function test_remove_pm()
+	{
+		$this->login();
+		
+		$this->admin_login();
+		$this->add_lang_ext('anavaro/pmsearch', 'info_acp_pmsearch');
+		$crawler = self::request('GET', 'adm/index.php?i=-anavaro-pmsearch-acp-acp_pmsearch_module&mode=main&sid=' . $this->sid);
+		
+		//$this->assertContains('11', $crawler->filter('#indexed_words')->text());
+		//$this->assertContains('14', $crawler->filter('#relative_indexes')->text());
+		$this->assertContains('alalaalalalalala', $crawler->text());
+		
+		$this->logout();
+	}
 }
 ?>
