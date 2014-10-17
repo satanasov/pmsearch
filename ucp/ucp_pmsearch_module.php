387bf4e034000144c5fcf56ae408d7db0eaa7ceb
Stanislav Atanasov <lucifer@anavaro.com>
2014-10-17 15:57:11 +0300

    Rework of search query to be optimized to work with Postgres

--- ucp/ucp_pmsearch_module.php
+++ ucp/ucp_pmsearch_module.php
@@ -24,7 +24,7 @@ class ucp_pmsearch_module
 	}
 	function main($id, $mode)
 	{
-		global $db, $user, $auth, $template, $cache, $request, $phpbb_container;
+		global $db, $user, $auth, $template, $cache, $request, $phpbb_container, $user_loader;
 		global $config, $SID, $phpbb_root_path, $phpbb_admin_path, $phpEx, $k_config, $table_prefix;
 		//$this->var_display($action);
 		switch ($mode)
@@ -64,33 +64,57 @@ class ucp_pmsearch_module
 					{
 						// Let's get additional info
 						$page_array = array();
-						$sql_array = 'SELECT msg.msg_id as msg_id, msg.message_subject as msg_subject, msg.message_text as msg_text, msg.message_time as msg_time, msg.author_id as msg_author, u.user_id, u.username as msg_author_uname, u.user_colour as msg_author_colour, tmsg.msg_id, MAX(tmsg.pm_unread) as unread, MAX(tmsg.pm_replied) as replied
-								FROM ' . PRIVMSGS_TABLE . ' as msg, ' . USERS_TABLE . ' as u, ' . PRIVMSGS_TO_TABLE . ' as tmsg
-								WHERE u.user_id = msg.author_id and (msg.msg_id = tmsg.msg_id and msg.author_id = tmsg.author_id) and ' . $db->sql_in_set('msg.msg_id', $id_ary) . '
-								GROUP BY msg.msg_id, u.user_id, tmsg.msg_id
-								ORDER BY msg.msg_id DESC';
-						//$sql = $db->sql_build_query('SELECT', $sql_array);
-						$result = $db->sql_query($sql_array);
+						// As there seems to be some problem with POSTGRES SQL I'll split the info query in two separate queries - one for the message ...
+						$sql_array = array(
+							'SELECT'	=> 'msg.msg_id as msg_id, msg.message_subject as msg_subject, msg.message_time as msg_time, msg.author_id as msg_author, MAX(tmsg.pm_unread) as unread, MAX(tmsg.pm_replied) as replied',
+							'FROM'	=> array(
+								PRIVMSGS_TABLE => 'msg',
+								PRIVMSGS_TO_TABLE => 'tmsg'
+							),
+							'WHERE'	=> 'msg.msg_id = tmsg.msg_id and msg.author_id = tmsg.author_id and ' . $db->sql_in_set('msg.msg_id', $id_ary),
+							'GROUP_BY'	=> 'msg.msg_id',
+							'ORDER_BY'	=> 'msg.msg_id DESC'
+						);
+						$sql = $db->sql_build_query('SELECT', $sql_array);
+						//$this->var_display($sql);
+						$result = $db->sql_query($sql);
 						// Let's populate template
-						$count = 1;
+						$author_uid_arrray = array();
 						while ($row = $db->sql_fetchrow($result))
 						{
-						/*	$page_array[$row['msg_id']] = array(
+							$author_uid_arrray[] = $row['msg_author'];
+							$page_array[$row['msg_id']] = array(
 								'msg_id'	=> $row['msg_id'],
 								'msg_subject'	=>	$row['msg_subject'],
 								'msg_author'	=>	$row['msg_author'],
 								'msg_time'	=>	$user->format_date($row['msg_time']),
-								'msg_author_uname'	=> $row['msg_author_uname'],
-								'msg_author_colour'	=> $row['msg_author_colour']
-							);*/
+								'unread'	=> $row['unread'],
+								'replied'	=> $row['replied']
+							);
+						}
+						$db->sql_freeresult($result);
+						// ... one for the authors on this page
+						$authors_array = array();
+						$sql = 'SELECT user_id, username, user_colour FROM ' . USERS_TABLE . ' WHERE ' .  $db->sql_in_set('user_id', $author_uid_arrray);
+						$result = $db->sql_query($sql);
+						while ($row = $db->sql_fetchrow($result))
+						{
+							$authors_array[$row['user_id']] = array(
+								'username'	=> $row['username'],
+								'user_colour'	=> $row['user_colour']
+							);
+						}
+						$count = 1;
+						foreach ($page_array as $VAR) {
+							
 							$template->assign_block_vars('pm_results', array(
 								'S_ROW_COUNT'	=> $count,
-								'FOLDER_IMG_STYLE'	=> ($row['unread'] ? 'pm_unread' : 'pm_read'),
-								'PM_CLASS'	=> ($row['replied'] ? 'pm_replied_colour' : ''),
-								'U_VIEW_PM'	=> './ucp.php?i=pm&mode=view&p=' . $row['msg_id'],
-								'SUBJECT'	=> $row['msg_subject'],
-								'SENT_TIME'	=>	$user->format_date($row['msg_time']),
-								'MESSAGE_AUTHOR_FULL'	=> ($row['msg_author_colour'] ? '<a href="./memberlist.php?mode=viewprofile&u=' . $row['msg_author'] . '" class="username-coloured" style="color: #' . $row['msg_author_colour'] . ';">' . $row['msg_author_uname'] . '</a>' : '<a href="./memberlist.php?mode=viewprofile&u=' . $row['msg_author'] . '" class="username">' . $row['msg_author_uname'] . '</a>'),
+								'FOLDER_IMG_STYLE'	=> ($VAR['unread'] ? 'pm_unread' : 'pm_read'),
+								'PM_CLASS'	=> ($VAR['replied'] ? 'pm_replied_colour' : ''),
+								'U_VIEW_PM'	=> './ucp.php?i=pm&mode=view&p=' . $VAR['msg_id'],
+								'SUBJECT'	=> $VAR['msg_subject'],
+								'SENT_TIME'	=>	$user->format_date($VAR['msg_time']),
+								'MESSAGE_AUTHOR_FULL'	=> ($authors_array[$VAR['msg_author']]['user_colour'] ? '<a href="./memberlist.php?mode=viewprofile&u=' . $VAR['msg_author'] . '" class="username-coloured" style="color: #' . $authors_array[$VAR['msg_author']]['user_colour'] . ';">' . $authors_array[$VAR['msg_author']]['username'] . '</a>' : '<a href="./memberlist.php?mode=viewprofile&u=' . $VAR['msg_author'] . '" class="username">' . $authors_array[$VAR['msg_author']]['username'] . '</a>'),
 							));
 							$count ++;
 						}
